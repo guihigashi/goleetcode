26ed73cb73b0337f99ef7d45ed1c17b6bcb9ab77
Guilherme Higashi <guihigashi@gmail.com>
2024-12-23 15:30:32 -0400

    add 7. Reverse Integer

---
+++ _007/reverse.go
@@ -0,0 +1,17 @@
+package _007
+
+import "math"
+
+func reverse(x int) int {
+	ans := 0
+
+	for ; x != 0; x /= 10 {
+		ans = ans*10 + x%10
+	}
+
+	if ans < math.MinInt32 || ans > math.MaxInt32 {
+		return 0
+	}
+
+	return ans
+}
