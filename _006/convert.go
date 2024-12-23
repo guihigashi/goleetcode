d54ca2cf00a38655d8193b2a01663d6fbae98bab
Guilherme Higashi <guihigashi@gmail.com>
2024-12-23 14:30:49 -0400

    add 6. Zigzag Conversion

---
+++ _006/convert.go
@@ -0,0 +1,33 @@
+package _006
+
+import (
+	"bytes"
+	"strings"
+)
+
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+
+	rows := make([]bytes.Buffer, numRows)
+
+	k := 0
+	direction := -1
+
+	for _, c := range s {
+		rows[k].WriteRune(c)
+		if k == 0 || k == numRows-1 {
+			direction *= -1
+		}
+		k += direction
+	}
+
+	var sb strings.Builder
+
+	for _, row := range rows {
+		sb.Write(row.Bytes())
+	}
+
+	return sb.String()
+}
